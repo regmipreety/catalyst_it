1c43022a5450ce318994e30ed949dc7ec1eece03
regmipreety <priety.regmi46@gmail.com>
2024-03-21 15:27:18 -0500

    merging feature branch to develop

---
+++ user_upload.php
@@ -0,0 +1,33 @@
+<?php
+$status = "Error importing csv files \n";
+$userInput = $argv[1];
+switch ($userInput){
+    case '--file':
+        echo "CSV file name: $filename\n";
+    // Call the function to parse the CSV file
+        include "parse_csv.php";
+        break;
+    case '--create_table':
+        echo "creating table...";
+        include "create_table.php";
+        break;
+    case '-u':
+        echo "root";
+        break;
+    case '-p':
+        echo "";
+        break;
+    case '-h';
+        echo "localhost";
+        break;
+    case '--help':
+        include "display_list.php";
+        break;
+        
+    default:
+        echo "invalid argument provided";
+
+}
+
+    
+?>
