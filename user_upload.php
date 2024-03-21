dca36a12b97737f759cad5a14a05d0dda16908e3
regmipreety <priety.regmi46@gmail.com>
2024-03-21 11:45:27 -0500

    saving csv rows to users table

--- user_upload.php
+++ user_upload.php
@@ -1,24 +1,19 @@
 <?php
+$status = "Error importing csv files";
+$filename = $argv[1];
+
+//get the filename
 include_once "db_connect.php";
-$lines = file('users.csv', FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
-$csv = array_map('str_getcsv', $lines);
 
-$col_names = array_shift($csv);
+    $file = fopen($filename, "r");
+    while (($emapData = fgetcsv($file, 10000, ",")) !== FALSE)
+    {
+        $sql= "INSERT into users(name, surname, email) values ('$emapData[0]','$emapData[1]','$emapData[2]')";
+        mysqli_query($conn, $sql);
+    }
+fclose($file);
+$status = "CSV has been successfully updated";
 
-
-$users = [];
-$stmt = 'INSERT INTO users (name, surname, email) VALUES (?,?,?)';
-mysqli_query($conn, $stmt) or die(mysqli); 
-foreach($csv as $row) {
-    $users[] = [
-        $col_names[0] => ucfirst(strtolower($row[0])),
-        $col_names[1] => ucfirst(strtolower($row[1])),
-        $col_names[2] => strtolower(strtolower($row[2])),
-    ];
-}
-
-foreach($users as $user) {
-    $stmt->execute($user);
-}
+echo $status;
 
 ?>
